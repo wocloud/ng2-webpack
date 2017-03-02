2e24c8ac593ba9fbea23ff2ac0d72cfd88a9be53
sophia.wang <wangpp@chinaskycloud.com>
2017-03-02 16:53:59 +0800

    remove js/map file out of control

--- src/app/app.menu.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pages_menu_1 = require("./pages/pages.menu");
-exports.MENU = pages_menu_1.PAGES_MENU.slice();
-//# sourceMappingURL=app.menu.js.map
--- src/app/app.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-exports.routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    { path: '**', redirectTo: 'dashboards' }
-];
-exports.routing = router_1.RouterModule.forRoot(exports.routes, { useHash: true });
-//# sourceMappingURL=app.routing.js.map
